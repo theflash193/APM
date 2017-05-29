07854a152311fee1b817bae02a6fb51e07ffb09b
Gordon RASS-KWASI <grass-kw@e1r1p8.42.fr>
2017-05-29 17:31:51 +0200

    correction probleme d'affichage

--- ex00/APM/APM/ViewController.swift
+++ ex00/APM/APM/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource  {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  {
     let URLRessource = ["https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/a3411.jpg?itok=oxF0ZLL0", "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia14454.jpg?itok=6C1bhU6q", "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/potw1652a.jpg?itok=5osF5N3n", "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/aurora_vir_2016357_lrg.jpg?itok=t_C47OWx"]
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
     }
 
@@ -28,6 +30,7 @@ class ViewController: UIViewController, UICollectionViewDataSource  {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        print("hello world")
         return cell
     }
     
